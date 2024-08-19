12ed06e5e57403ade25b10d4e6de9d54e30e237d
tMiy4 <kuma424dochi@g.ecc.u-tokyo.ac.jp>
2024-08-19 11:39:06 +0900

    infer_states_vfe_policiesで求めたqs_piからBayesian model averagingを計算する関数：calc_bayesian_model_averagingを実装しました．

--- pymdp/jax/agent.py
+++ pymdp/jax/agent.py
@@ -1162,3 +1162,14 @@ class Agent(Module):
         #self.gamma=gamma
         agent = tree_at(lambda x: x.gamma, agent, gamma)
         return agent, q_pi, q_pi_0, gamma, Gerror
+    
+    def calc_bayesian_model_averaging(self, qs_pi):
+        if len(qs_pi)>1:
+            beliefs = jnp.array(qs_pi)
+            Bayesian_model_avaraging_full = jnp.mean(beliefs, axis=0)
+            Bayesian_model_avaraging_full = list(Bayesian_model_avaraging_full)
+            K, t,_= self.policies.shape
+            Bayesian_model_avaraging = jtu.tree_map( lambda x: x[:, :-t], Bayesian_model_avaraging_full)
+        else:
+            Bayesian_model_avaraging=qs_pi
+        return Bayesian_model_avaraging
